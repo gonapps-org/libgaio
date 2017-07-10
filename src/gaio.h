179435342d3616a5c7c3beac02897c604145f979
Byeonggon Lee <gonny952@gmail.com>
2017-07-10 10:03:59 +0000

    Commit

--- src/gaio.h
+++ src/gaio.h
@@ -21,13 +21,13 @@ int gaio_Nop_fcntl(struct gaio_Io* io, int command, int argCount, ...);
 int gaio_Nop_fileno(struct gaio_Io* io);
 int gaio_Nop_close(struct gaio_Io* io);
 
-#define GAIO_INIT_NOP(io)         \
-io->read = gaio_Nop_read;         \
-io->write = gaio_Nop_write;       \
-io->sendfile = gaio_Nop_sendfile; \
-io->fcntl = gaio_Nop_fcntl;       \
-io->fileno = gaio_Nop_fileno;     \
-io->close = gaio_Nop_close
+#define GAIO_INIT_NOP(io)           \
+(io)->read = gaio_Nop_read;         \
+(io)->write = gaio_Nop_write;       \
+(io)->sendfile = gaio_Nop_sendfile; \
+(io)->fcntl = gaio_Nop_fcntl;       \
+(io)->fileno = gaio_Nop_fileno;     \
+(io)->close = gaio_Nop_close
 
 int gaio_FdPointer_read(struct gaio_Io* io, void* buffer, int readSize);
 int gaio_FdPointer_write(struct gaio_Io* io, void* buffer, int writeSize);
