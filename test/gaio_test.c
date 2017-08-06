9bbda52033781e2ea5d18ffed7f0fae6bd172f58
Byeonggon Lee <gonny952@gmail.com>
2017-08-06 05:06:03 +0000

    Hmm

--- test/gaio_test.c
+++ test/gaio_test.c
@@ -5,23 +5,23 @@
 #include <stdlib.h>
 int main() {
     struct gaio_Io io;
-    struct gaio_Io_Callbacks callbacks;
-    GAIO_NOP_INIT(&callbacks);
-    io.callbacks = &callbacks;
+    struct gaio_Io_Methods methods;
+    GAIO_NOP_INIT(&methods);
+    io.methods = &methods;
     int fd = STDOUT_FILENO;
     io.object.integer = fd;
-    io.callbacks->write = gaio_Fd_write;
-    io.callbacks->fileno = gaio_Fd_fileno;
+    io.methods->write = gaio_Fd_write;
+    io.methods->fileno = gaio_Fd_fileno;
     char* buf = "HELLO WORLD!\n";
-    io.callbacks->write(&io, buf, sizeof("HELLO WORLD!\n") - 1);
-    io.callbacks->sendfile = gaio_Generic_sendfile;
+    io.methods->write(&io, buf, sizeof("HELLO WORLD!\n") - 1);
+    io.methods->sendfile = gaio_Generic_sendfile;
     int self = open("gaio_test.c", O_RDONLY);
     struct gaio_Io selfIo;
     selfIo.object.pointer = &self;
-    selfIo.callbacks = &callbacks;
-    io.callbacks->sendfile(&io, &selfIo, NULL, 1024);
-    io.callbacks->close = gaio_Nop_close;
-    io.callbacks->close(&io);
-    io.callbacks->fcntl = gaio_Fd_fcntl;
-    io.callbacks->fcntl(&io, F_SETFL, io.callbacks->fcntl(&io, F_GETFL, 0) | O_NONBLOCK);
+    selfIo.methods = &methods;
+    io.methods->sendfile(&io, &selfIo, NULL, 1024);
+    io.methods->close = gaio_Nop_close;
+    io.methods->close(&io);
+    io.methods->fcntl = gaio_Fd_fcntl;
+    io.methods->fcntl(&io, F_SETFL, io.methods->fcntl(&io, F_GETFL, 0) | O_NONBLOCK);
 };
