878ddfaf6ee467d5d8c2ff8ac9ab7767b82fe2c4
Byeonggon Lee <gonny952@gmail.com>
2017-08-21 10:12:54 +0000

    Commit

--- test/gaio_test.c
+++ test/gaio_test.c
@@ -1,9 +1,15 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
 #include <fcntl.h>
 #include "../src/gaio.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+
+static void gaio_test(void** state) {
     struct gaio_Io io;
     struct gaio_Io_Methods methods;
     GAIO_NOP_INIT(&methods);
@@ -15,7 +21,7 @@ int main() {
     char* buf = "HELLO WORLD!\n";
     io.methods->write(&io, buf, sizeof("HELLO WORLD!\n") - 1);
     io.methods->sendfile = gaio_Generic_sendfile;
-    int self = open("gaio_test.c", O_RDONLY);
+    int self = open(__FILE__, O_RDONLY);
     struct gaio_Io selfIo;
     selfIo.object.pointer = &self;
     selfIo.methods = &methods;
@@ -24,4 +30,11 @@ int main() {
     io.methods->close(&io);
     io.methods->fcntl = gaio_Fd_fcntl;
     io.methods->fcntl(&io, F_SETFL, io.methods->fcntl(&io, F_GETFL, 0) | O_NONBLOCK);
-};
+}
+
+int main() {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(gaio_test),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
